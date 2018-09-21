122fccf2c5ff7187af3a0176095677ced151ada8
Tr-Heath <travis.heath@outlook.com>
2018-09-20 22:40:14 -0700

    Add gene info and dependent files

---
+++ Camo_GA_Training/Assets/DNA.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour {
+
+	//gene for color, floats for Red, Green, Blue
+	public struct GeneColor {
+		public float r; 
+		public float g; 
+		public float b;
+		public GeneColor(float redValue, float greenValue, float blueValue){
+			r = redValue;
+			g = greenValue;
+			b = blueValue;
+		}
+	};
+
+	bool dead = false;
+	public float timeTillDeath = 0;
+	Collider2D sCollider;
+	SpriteRenderer sRenderer;
+	public GeneColor color;
+	void OnMouseDown() {
+		dead = true;
+		timeTillDeath = PopulationManager.elapsed;
+		// Debug.Log("Dead at: " + timeTillDeath);
+		sRenderer.enabled = false;
+		sCollider.enabled = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		sRenderer = GetComponent<SpriteRenderer>();
+		sCollider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
